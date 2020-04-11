22338289400cef841760760af5ef005c30fe3fcb
Ashu <Ashu@users.noreply.github.com>
2020-04-11 17:15:06 +0530

    Initial Commit

---
+++ iMultiply/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iMultiply
+//
+//  Created by Ashutosh Dave on 11/04/20.
+//  Copyright © 2020 Ashutosh Dave. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
